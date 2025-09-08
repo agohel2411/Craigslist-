a51021c06819819f70a40b553e9370bd7a6a910b
Ayush Gohel <earningvip8@gmail.com>
2025-09-08 12:54:53 +0530

    task 4 done by json

--- main.py
+++ main.py
@@ -55,6 +55,32 @@ def multifilter(filter: str, upper: int | None = None, lower: int | None = None,
             if distance<radius:
                 lst.append(person)
 
-        return len(lst)
+        return lst
         
-        
+@app.get("/radius")
+def radius(radius: float, latitude: float, longitude: float):
+    R = 6371
+
+    lat1_rad = math.radians(latitude)
+    lon1_rad = math.radians(longitude)
+    lst = []
+
+    for person in data:
+        coordinates = person['loc']
+        lat2_rad = math.radians(coordinates[0])
+        lon2_rad = math.radians(coordinates[1])
+
+        dlon = lon2_rad - lon1_rad
+        dlat = lat2_rad - lat1_rad
+
+        # Haversine formula
+        a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        distance = R * c
+            
+
+        if distance<radius:
+            lst.append(person)
+
+    return len(lst)
