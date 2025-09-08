cd091256973d7e29767b1120563f0d5be714a889
Ayush Gohel <earningvip8@gmail.com>
2025-09-08 12:48:51 +0530

    task 5 done by json

--- main.py
+++ main.py
@@ -19,3 +19,42 @@ def singleitem(id: str | None = None, long: float | None = None, lat: float | No
 @app.get("/list")
 def listitem(status: str | None = None, userId: str | None = None):
     return [person for person in data if ((person['status']==status) or (person['userId']==userId))]
+
+import math
+@app.get("/multifilter")
+def multifilter(filter: str, upper: int | None = None, lower: int | None = None, words: str | None = None, radius: float | None = None, latitude: float | None = None, longitude: float | None = None):
+    if filter == 'price':
+        return [person for person in data if ((person[filter]>lower) and (person[filter]<upper))]
+    elif filter == 'desc':
+        for person in data:
+            if person['description'] != None:
+                if words in person['description']:
+                    return person
+    elif filter == 'radius':
+        R = 6371
+
+        lat1_rad = math.radians(latitude)
+        lon1_rad = math.radians(longitude)
+        lst = []
+
+        for person in data:
+            coordinates = person['loc']
+            lat2_rad = math.radians(coordinates[0])
+            lon2_rad = math.radians(coordinates[1])
+
+            dlon = lon2_rad - lon1_rad
+            dlat = lat2_rad - lat1_rad
+
+            # Haversine formula
+            a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+            distance = R * c
+            
+
+            if distance<radius:
+                lst.append(person)
+
+        return len(lst)
+        
+        
