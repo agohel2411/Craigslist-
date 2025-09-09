0a6e8155e22e7782de34e0bf05cd32eb7912b9f3
Ayush Gohel <earningvip8@gmail.com>
2025-09-09 19:23:07 +0530

    logs added in app.py (json)

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from src.model import Sales
 from src.database import SessionLocal, engine
 from fastapi import HTTPException
 from sqlalchemy import desc, or_, and_
+from src.log import logger
 
 model.Base.metadata.create_all(bind=engine)
 
@@ -26,43 +27,60 @@ app = FastAPI()
 with open("data/sale.json") as f:
     data = json.load(f)
 
+logger.info(f"API Loaded")
+
 @app.get("/getsorteddata", tags=['Json'], status_code=status.HTTP_202_ACCEPTED)
 def price_sorted(reverse: bool, criteria: str):
+    logger.info(f"Hitted /getsorteddata (json): reverse = {reverse}, criteria = {criteria}")
     try:
         sorteddata = sorted(data, key=lambda x: x[criteria], reverse=reverse)
 
         if sorteddata:
+            logger.info(f"Successfully returned {'reversed' if reverse else ''} sorted data by {criteria} in /getsorteddata (json)")
             return sorteddata
         else:
+            logger.error(f"Error Raised in /getsorteddata: 404 - No data found (josn)")
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No data found")
 
     except KeyError:
+        logger.error(f"Criteria provided ({criteria}) in /getsorteddata was invalid (json)")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid sort criteria: '{criteria}'")
 
     except Exception as e:
+        logger.error(f"Error raised in /getsorteddata: {str(e)} (json)")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 @app.get("/getitem", tags=['Json'], status_code=status.HTTP_202_ACCEPTED)
 def singleitem(id: str | None = None, long: float | None = None, lat: float | None = None):
+    logger.info(f"Hitted /getitem (json): {f'id = {id}' if id  else f'lattitude = {lat}, longitude = {long}'}")
     try:
         if (not id) or (not lat) or (not long):
+            logger.error("Any parameter not provided in /getitem (josn)")
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid criteria: Provide any one parameter to proceed")
         
+        logger.info(f"Successfully returned data containing {'id' if id else 'location'} in /getitem (json)")
         return [person for person in data if ((person['id']==id) or (person['loc']==[long,lat]))]
     except Exception as e:
+        logger.error(f"Error raised in /getitem: {str(e)} (json)")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 @app.get("/getitemlist", tags=['Json'], status_code=status.HTTP_202_ACCEPTED)
 def listitem(status: str | None = None, userid: str | None = None):
+    logger.info(f"Hitted /getitemlist (json): {f'status = {status}' if status else f'criteria = {userid}'}")
     try:
-        if (not status) and (not userid):
+        if (not status) or (not userid):
+            logger.error("Any parameter not provided in /getitemlist")
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Criteria: Provide any one parameter (status -- or -- userid) to proceed")
-        return [person for person in data if ((person['status']==status) or (person['userId']==userid))]
+        
+        logger.info(f"Successfully returned data of having {status} status and {userid} userId in /getitemlist (josn)")
+        return [person for person in data if ((person['status']==status) or (person['userId']==userid) if status or userid else (person['status']==status) and (person['iserId']==userid))]
     except Exception as e:
+        logger.error(f"Error raised in /getitemlist: {str(e)} (json)")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 @app.get("/get_items_in_radius", tags=['Json'], status_code=status.HTTP_202_ACCEPTED)
 def radius(radius: float, latitude: float, longitude: float):
+    logger.info(f"Hitted /get_items_in_radius (json): radius = {radius}, latitude = {latitude}, longitude = {longitude}")
     try:
         R = 6371
 
@@ -87,24 +105,34 @@ def radius(radius: float, latitude: float, longitude: float):
 
             if distance<radius:
                 lst.append(person)
-
+        logger.info(f"Successfully returned datapoints within radius of {radius}km from location [{latitude},{longitude}] in /get_item_in_radius (json)")
         return len(lst)
     except Exception as e:
+        logger.error(f"Error raised in /get_item_by_radius: {str(e)} (json)")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 @app.get("/get_items_by_filter", tags=['Json'], status_code=status.HTTP_202_ACCEPTED)
 def multifilter(filterby: str, upper: int | None = None, lower: int | None = None, words: str | None = None, radius: float | None = None, latitude: float | None = None, longitude: float | None = None):
+
     try:    
         if filterby == 'price':
+            logger.info(f"Hitted /get_items_by_filter (json): filter = {filterby}, upper = {upper}, lower = {lower}")
             if lower>upper:
+                logger.error(f"Upper value {upper} less than lower value {lower} in /get_items_by_filter (json)")
                 raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid parameters: Upper value cannot be less than lower")
+            
+            logger.info(f"Successfully data returned having prices from {lower} to {upper} in /get_items_by_filter (json)")
             return [person for person in data if ((person[filter]>lower) and (person[filter]<upper))]
+        
         elif filterby == 'desc':
+            logger.info(f"Hitted /get_items_by_filter (json): filter = {filterby}, words = {words}")
             for person in data:
                 if person['description'] != None:
                     if words in person['description']:
+                        logger.info(f"Successfully returned data containing {words} in description from /get_items_by_filter (json)")
                         return person
         elif filterby == 'radius':
+            logger.info(f"Hitted /get_items_by_filter (json): filter = {filterby}, radius = {upper}, lower = {lower}")
             R = 6371
 
             lat1_rad = math.radians(latitude)
@@ -128,10 +156,13 @@ def multifilter(filterby: str, upper: int | None = None, lower: int | None = Non
                 if distance<radius:
                     lst.append(person)
 
+            logger.info(f"Successfully returned datapoints within radius of {radius}km from location [{latitude},{longitude}] in /get_items_by_filter (json)")
             return lst
         else:
+            logger.error(f"Invalid filter value ({filterby}) provided in /get_items_by_filter (json)")
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Parameter: Please provide valid filter")
     except Exception as e:
+        logger.error(f"Error raised in /get_items_by_filter: {str(e)} (json)")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
         
 
--- src/log.py
+++ src/log.py
@@ -4,7 +4,7 @@ logging.basicConfig(
     level = logging.INFO,
     format= "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt= "%Y-%m-%d %H:%M:%S",
-    handlers = [logging.FileHandler('app.log'), logging.StreamHandler()]
+    handlers = [logging.FileHandler('app.log')]
 )
 
 logger = logging.getLogger(__name__)
