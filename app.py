dcec9bad306834d538bf095b2570ee8d9c688aff
Ayush Gohel <earningvip8@gmail.com>
2025-09-09 14:49:28 +0530

    task 4 json modification done

--- app.py
+++ app.py
@@ -59,6 +59,37 @@ def listitem(status: str | None = None, userid: str | None = None):
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
+@app.get("/get_items_in_radius", tags=['Json'], status_code=status.HTTP_202_ACCEPTED)
+def radius(radius: float, latitude: float, longitude: float):
+    try:
+        R = 6371
+
+        lat1_rad = math.radians(latitude)
+        lon1_rad = math.radians(longitude)
+        lst = []
+
+        for person in data:
+            coordinates = person['loc']
+            lat2_rad = math.radians(coordinates[0])
+            lon2_rad = math.radians(coordinates[1])
+
+            dlon = lon2_rad - lon1_rad
+            dlat = lat2_rad - lat1_rad
+
+            # Haversine formula
+            a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+            distance = R * c
+                
+
+            if distance<radius:
+                lst.append(person)
+
+        return len(lst)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
 @app.get("/get_items_by_filter", tags=['Json'], status_code=status.HTTP_202_ACCEPTED)
 def multifilter(filterby: str, upper: int | None = None, lower: int | None = None, words: str | None = None, radius: float | None = None, latitude: float | None = None, longitude: float | None = None):
     try:    
@@ -99,36 +130,8 @@ def multifilter(filterby: str, upper: int | None = None, lower: int | None = Non
         else:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Parameter: Please provide valid filter")
     except Exception as e:
-         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
         
-@app.get("/radius", tags=['Json'])
-def radius(radius: float, latitude: float, longitude: float):
-    R = 6371
-
-    lat1_rad = math.radians(latitude)
-    lon1_rad = math.radians(longitude)
-    lst = []
-
-    for person in data:
-        coordinates = person['loc']
-        lat2_rad = math.radians(coordinates[0])
-        lon2_rad = math.radians(coordinates[1])
-
-        dlon = lon2_rad - lon1_rad
-        dlat = lat2_rad - lat1_rad
-
-        # Haversine formula
-        a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-        distance = R * c
-            
-
-        if distance<radius:
-            lst.append(person)
-
-    return len(lst)
-
 
 
 @app.get("/pricesorteddb", tags=['SQL Db'])
